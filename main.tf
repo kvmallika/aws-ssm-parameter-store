90eecffd971d6b51f8fdbb6e3c4e25d509f4320b
Mallika <kmallika.malli@gmail.com>
2023-07-04 14:08:39 +0530

    parameter creation in aws

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "hello from terraform"
+  key_id = "8ad7b83e-d322-4cb4-a3cf-f1210e2a762a"
+
 }
