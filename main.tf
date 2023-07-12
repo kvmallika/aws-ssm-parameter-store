bc52de64b184eb8d6d0f07af1ec17e1256806cc5
Mallika <kmallika.malli@gmail.com>
2023-07-12 14:57:33 +0530

    password variable

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ resource "aws_ssm_parameter" "parameters" {
   count= length(var.parameters)
   name  = var.parameters[count.index].name
   value  = var.parameters[count.index].value
+  type  = "String"
+  key_id = "8ad7b83e-d322-4cb4-a3cf-f1210e2a762a"
+}
+
+resource "aws_ssm_parameter" "passwords" {
+  count= length(var.parameters)
+  name  = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
   type  = "SecureString"
   key_id = "8ad7b83e-d322-4cb4-a3cf-f1210e2a762a"
 }
+
